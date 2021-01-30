903281d6f7c36033740a824edf4b5d9be9c5af2c
Javier Soler Artero <javier.soler.jsa@gmail.com>
2021-01-30 12:10:35 +0100

    Update chat

--- .github/actions/telegram_action/index.js
+++ .github/actions/telegram_action/index.js
@@ -2,7 +2,6 @@ const TelegramBot = require("node-telegram-bot-api");
 const core = require("@actions/core");
 
 const token = core.getInput("token");
-
 const bot = new TelegramBot(token, { polling: true });
 
 bot.sendMessage(core.getInput("chat"), core.getInput("message"));
