a8230c89c07255df482bf41ded8b7642ce0cd2d0
Javier Soler Artero <javier.soler.jsa@gmail.com>
2021-02-01 15:51:42 +0100

    telegram mejorado

--- .github/actions/telegram_action/index.js
+++ .github/actions/telegram_action/index.js
@@ -1,11 +1,19 @@
 const TelegramBot = require("node-telegram-bot-api");
 const core = require("@actions/core");
+const github = require("@actions/github");
 
 const token = core.getInput("token");
 const bot = new TelegramBot(token, { polling: false });
 
 try {
-  bot.sendMessage(core.getInput("chat"), core.getInput("message"));
+  let message = core.getInput("message");
+  message += `\n
+  GitHub Information:\n
+  - Commit name: ${git.head_commit.author.name}\n
+  - Commit message: ${git.head_commit.message}\n
+  - Username: ${git.head_commit.author.username}\n
+  - Email: ${git.head_commit.author.email}\n`;
+  bot.sendMessage(core.getInput("chat"), message);
 } catch (error) {
   core.setFailed(error.message);
 }
